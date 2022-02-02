7d62c52c27b58c56590ab5e6cb4b95ecd5ebbda2
Danpa_cho <danpa725@cau.ac.kr>
2022-02-02 15:17:54 +0900

    Make: basic UI atomic components.

--- components/UI/Atoms/Input/Input.tsx
+++ components/UI/Atoms/Input/Input.tsx
@@ -1,7 +1,84 @@
-import React from "react"
+import React, { useEffect, useRef, useState } from "react"
+import styled from "styled-components"
+import Box from "../Box/Box"
+import Button from "../Button/Button"
 
-function Input() {
-    return <div></div>
+interface InputProps {
+    inputTitle: string
+    placeholder: string
+    minLength: number
+    maxLength: number
+    defaultValue?: string
+    onSubmit: () => void
+    //* useState hook required
+    setText: (text?: string) => void
+}
+
+const InputContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    gap: 1rem;
+
+    width: fit-content;
+    height: fit-content;
+    padding: 0.15rem;
+`
+
+const InputStyled = styled.input`
+    width: 100%;
+    height: 100%;
+
+    ::placeholder {
+        color: ${(p) => p.theme.gray3};
+        font-weight: 400;
+    }
+
+    background-color: transparent;
+`
+
+function Input({
+    inputTitle,
+    placeholder,
+    minLength,
+    maxLength,
+    defaultValue,
+    onSubmit,
+    setText,
+}: InputProps) {
+    const inputRef = useRef<HTMLInputElement>(null)
+
+    useEffect(() => {
+        inputRef.current?.focus()
+    }, [])
+
+    return (
+        <Box
+            innerContent={
+                <InputContainer>
+                    <InputStyled
+                        type="text"
+                        ref={inputRef}
+                        title={inputTitle}
+                        placeholder={placeholder}
+                        minLength={minLength}
+                        maxLength={maxLength}
+                        defaultValue={defaultValue}
+                        onSubmit={() => {
+                            setText(inputRef.current?.value)
+                            onSubmit()
+                        }}
+                    />
+                    <Button
+                        buttonStyleType="default"
+                        innerContent={<p>{"->"}</p>}
+                        onClick={onSubmit}
+                    />
+                </InputContainer>
+            }
+        />
+    )
 }
 
 export default Input
--- components/UI/Atoms/Tag/Tag.tsx
+++ components/UI/Atoms/Tag/Tag.tsx
@@ -1,6 +1,19 @@
-import React from "react"
+import styled from "styled-components"
 
-function Tag() {
+const TagStyled = styled.span`
+    width: fit-content;
+    height: fit-content;
+    padding: 0.1rem;
+    margin: 0.25rem;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+
+interface TagProps {}
+
+function Tag({}: TagProps) {
     return <div></div>
 }
 
