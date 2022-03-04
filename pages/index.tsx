6d6ec0bab34dbcd14a58610093712d0ee8e2751c
June@MacBook <danpa725@cau.ac.kr>
2022-03-04 23:09:01 +0900

    Update: main page.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
-import CategoryLink from "@/components/Post/Main/CategoryLink/CategoryLink"
-import LatestPostLink from "@/components/Post/Main/LatestPostLink/LatestPostLink"
+import CategoryLink from "@/components/Blog/Main/CategoryLink/CategoryLink"
+import LatestPostLink from "@/components/Blog/Main/LatestPostLink/LatestPostLink"
 import MainBackground from "@/components/UI/Atoms/Background/Mainbackground"
 import media from "@/styles/utils/media"
 import { getCategoryInfoArray } from "@/utils/function/blog-contents-loader/category/getCategory"
@@ -9,7 +9,10 @@ import { PostMeta } from "@/utils/types/main/meta"
 import { GetStaticProps } from "next"
 import styled from "styled-components"
 
+export type PageType = "Home" | "Category" | "Post"
+
 const CONTAINER_INFO_COLOR = "#776350"
+//* Main
 const MainPageContainer = styled.div`
     width: 70%;
     height: 100%;
@@ -22,46 +25,37 @@ const MainPageContainer = styled.div`
     gap: 1rem;
 
     ${media.widePhone} {
-        width: fit-content;
+        width: 100%;
         flex-direction: column-reverse;
         align-items: center;
         justify-content: center;
 
-        gap: 2rem;
-
-        padding: 2.5rem 0;
+        padding: 2rem 0;
     }
 `
-
-const CategoryLinkContainer = styled.div`
+//* Latest Post
+const LatestPostContainer = styled.div`
     display: flex;
     flex-direction: column;
-    align-items: flex-start;
-    justify-content: flex-start;
+    align-items: flex-end;
+    justify-content: center;
 
-    height: 35rem;
-    overflow-y: scroll;
+    flex: 6;
 
-    gap: 1.75rem;
+    gap: 1rem;
 
     ${media.widePhone} {
-        height: fit-content;
-        width: fit-content;
-        padding: 2.5rem;
-        overflow: auto;
         gap: 1.5rem;
-        padding-right: 0;
-        align-items: center;
-        justify-content: center;
+        align-items: flex-start;
     }
 `
-
 const LatestPostLinkContainer = styled.div`
     display: flex;
     flex-direction: column;
     align-items: flex-end;
     justify-content: flex-start;
 
+    width: 100%;
     height: 35rem;
     overflow-y: scroll;
 
@@ -85,28 +79,61 @@ const LatestPostLinkContainer = styled.div`
 
     ${media.widePhone} {
         height: fit-content;
-        overflow: auto;
-        gap: 1.5rem;
-        padding-right: 0;
+
         align-items: center;
         justify-content: center;
+
+        gap: 1.5rem;
+
+        padding-right: 0;
+        padding-bottom: 2rem;
+        overflow-y: auto;
     }
 `
-const LatestPostContainer = styled.div`
+
+//* Category
+const CategoryContainer = styled.div`
     display: flex;
     flex-direction: column;
-    align-items: flex-end;
+    align-items: flex-start;
     justify-content: center;
+
+    flex: 4;
 
     gap: 1rem;
 
     ${media.widePhone} {
+        width: inherit;
+    }
+`
+const CategoryLinkContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: flex-start;
+
+    width: 100%;
+    height: 35rem;
+    overflow-y: scroll;
+
+    gap: 1.75rem;
+
+    ${media.widePhone} {
+        height: fit-content;
+        width: inherit;
+
+        align-items: center;
+        justify-content: center;
+
         gap: 1.5rem;
-        align-items: flex-start;
+
+        overflow-y: auto;
+
+        padding-bottom: 2.5rem;
     }
 `
 
-const ContainerInfo = styled.div`
+const ContainerTitle = styled.div`
     font-size: ${(p) => p.theme.sm};
     font-weight: 400;
     color: ${CONTAINER_INFO_COLOR};
@@ -117,19 +144,12 @@ const ContainerInfo = styled.div`
     user-select: none;
 
     ${media.widePhone} {
-        margin-left: 10%;
+        border-radius: ${(p) => `0 ${p.theme.bsm} ${p.theme.bsm} 0`};
+        border-left: 0.25rem solid ${CONTAINER_INFO_COLOR};
+        margin-left: 5%;
     }
 `
-const CategoryContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    justify-content: center;
 
-    gap: 1rem;
-`
-
-export type PageType = "Home" | "Category" | "Post"
 interface MainPageProps {
     latestPostArray: PostMeta[]
     categoryInfoArray: CategoryInfo[]
@@ -140,7 +160,7 @@ function MainPage({ latestPostArray, categoryInfoArray }: MainPageProps) {
         <>
             <MainPageContainer>
                 <CategoryContainer>
-                    <ContainerInfo>Category</ContainerInfo>
+                    <ContainerTitle>Category</ContainerTitle>
                     <CategoryLinkContainer>
                         {categoryInfoArray.map((categoryInfo) => (
                             <CategoryLink
@@ -152,7 +172,7 @@ function MainPage({ latestPostArray, categoryInfoArray }: MainPageProps) {
                 </CategoryContainer>
 
                 <LatestPostContainer>
-                    <ContainerInfo>Latest Post</ContainerInfo>
+                    <ContainerTitle>Latest Post</ContainerTitle>
                     <LatestPostLinkContainer>
                         {latestPostArray.map((latestPost, order) => (
                             <LatestPostLink
