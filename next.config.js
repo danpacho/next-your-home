ba21b681a8db73b1f4050137892a7b75ad1e2017
June@MacBook <danpa725@cau.ac.kr>
2022-03-12 17:59:17 +0900

    Update: package.json -remove unused package -add script and update next.config.js .

--- next.config.js
+++ next.config.js
@@ -1,28 +1,23 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
+
 const { withPlugins } = require('next-compose-plugins')
 
 //* custome plugins ---------------------------------
-const withMDX = require('@next/mdx')({
-  extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [],
-    rehypePlugins: [],
-  },
-});
+
 
 //* next default config ------------------------------
 const nextConfig = {
-  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'tsx', 'ts'],
+  pageExtensions: ['mdx', 'tsx', 'ts'],
   reactStrictMode: true,
-  experimental: {
+  compiler: {
       styledComponents: true,
   },
 }
 
 module.exports = withPlugins(
-  [
-   withMDX
-  ],
+  [],
   nextConfig
 ) 
 
