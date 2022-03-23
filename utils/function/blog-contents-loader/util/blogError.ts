2490cd2e7a884ac611ddb4675bbbe3f24e680a59
June@MacBook <danpa725@cau.ac.kr>
2022-03-23 16:31:04 +0900

    Make: custome blog error class for better compile error info message .

---
+++ utils/function/blog-contents-loader/util/blogError.ts
@@ -0,0 +1,160 @@
+type ErrorType = "propertyError" | "fileExtractionError" | "additionalInfoError"
+
+interface BlogErrorConstructorProps {
+    errorType: ErrorType
+    errorNameDescription: string
+    message: string
+    customeErrorMessage?: string
+}
+
+class BlogError extends Error {
+    protected errorType: ErrorType
+
+    constructor({
+        errorType,
+        errorNameDescription,
+        message,
+        customeErrorMessage,
+    }: BlogErrorConstructorProps) {
+        super("")
+        this.errorType = errorType
+        this.name = `${this.messageDivider}[ 🛠 ERROR_OCCURED: ${this.errorType} -> ${errorNameDescription} 🛠 ]`
+        this.message = this.makeErrorMessage(message, customeErrorMessage)
+    }
+
+    protected messageDivider =
+        "\n\n----------------------------------------------------------------------------------------\n\n"
+    protected messageTab = "      "
+
+    protected makeErrorMessage = (
+        mainErrorMessage: string,
+        customeErrorMessage?: string
+    ) =>
+        !customeErrorMessage
+            ? `${this.messageDivider}${this.messageTab}❎ Error Occured, ${mainErrorMessage}${this.messageDivider}`
+            : `${this.messageDivider}${this.messageTab}❎ Error Occured, ${mainErrorMessage}\n${this.messageTab}❎ Check ${customeErrorMessage}${this.messageDivider}`
+}
+
+type PropertyType =
+    | "string"
+    | "number"
+    | "boolean"
+    | "symbol"
+    | "string[]"
+    | "number[]"
+    | "boolean[]"
+    | "symbol[]"
+
+type PropertyName<CustomeProperty> =
+    | Exclude<keyof CustomeProperty, keyof Array<any>>
+    | string
+interface BlogPropertyErrorConstructorProps<CustomeProperty>
+    extends Pick<
+        BlogErrorConstructorProps,
+        "customeErrorMessage" | "errorNameDescription"
+    > {
+    propertyName: PropertyName<CustomeProperty>
+    propertyType: PropertyType
+    errorPropertyValue?: string
+}
+
+class BlogPropertyError<CustomeProperty> extends BlogError {
+    constructor({
+        customeErrorMessage,
+        errorNameDescription,
+        propertyName,
+        propertyType,
+        errorPropertyValue,
+    }: BlogPropertyErrorConstructorProps<CustomeProperty>) {
+        const makePropertyErrorMessage = (
+            propertyName: PropertyName<CustomeProperty>,
+            propertyType: PropertyType,
+            errorPropertyValue?: string
+        ) => {
+            return `[${propertyName}] problem at: \n\n      ${propertyName}: ${String(
+                errorPropertyValue ?? propertyType
+            )} -> required❗️, not empty ${propertyType}❗️\n`
+        }
+
+        super({
+            errorType: "propertyError",
+            errorNameDescription,
+            message: makePropertyErrorMessage(
+                propertyName,
+                propertyType,
+                errorPropertyValue
+            ),
+            customeErrorMessage,
+        })
+    }
+}
+
+interface BlogErrorAdditionalInfoConstructorProps
+    extends Omit<BlogErrorConstructorProps, "errorType"> {
+    passedError: unknown
+}
+class BlogErrorAdditionalInfo extends BlogError {
+    private passedError: unknown
+    constructor({
+        passedError,
+        errorNameDescription,
+        message,
+        customeErrorMessage,
+    }: BlogErrorAdditionalInfoConstructorProps) {
+        super({
+            errorType: "additionalInfoError",
+            errorNameDescription,
+            message: "",
+            customeErrorMessage,
+        })
+
+        this.passedError = passedError
+
+        if (passedError instanceof BlogError) {
+            this.message = `${this.passedError}\n${this.messageTab}${message}\n${this.messageTab}${customeErrorMessage}${this.messageDivider}`
+        } else {
+            this.message = `${this.messageDivider}${this.messageTab}Unknown ERROR😓:\n\n${this.passedError}\n${this.messageTab}\n${this.messageTab}Please Follow [ Error Call Stack ] to find error.${this.messageDivider}`
+        }
+    }
+}
+
+type ReadingFileFormat = ".json" | ".txt" | ".mdx"
+interface BlogFileExtractionErrorConstructorProps
+    extends Omit<
+        BlogErrorConstructorProps,
+        "errorType" | "customeErrorMessage" | "message"
+    > {
+    encodingFormat?: BufferEncoding
+    readingFileFormat: ReadingFileFormat
+    readingFileName: string
+    readingFileLocation: string
+}
+
+class BlogFileExtractionError extends BlogError {
+    public readingFileLocation: string
+
+    constructor({
+        errorNameDescription,
+        readingFileLocation,
+        readingFileFormat,
+        encodingFormat = "utf-8",
+        readingFileName,
+    }: BlogFileExtractionErrorConstructorProps) {
+        const fileExtractionErrorMessage = encodingFormat
+            ? `while extracting ${readingFileName}${readingFileFormat}`
+            : `while extracting ${readingFileName}${readingFileFormat}, [ ✅ ENCODING FORMAT: ${encodingFormat} ]`
+
+        const fileLocationMessage = `file location🔎: \n\n  ${readingFileLocation}`
+
+        super({
+            errorType: "fileExtractionError",
+            errorNameDescription,
+            message: fileExtractionErrorMessage,
+            customeErrorMessage: fileLocationMessage,
+        })
+
+        this.readingFileLocation = readingFileLocation
+    }
+}
+
+export { BlogErrorAdditionalInfo, BlogPropertyError, BlogFileExtractionError }
