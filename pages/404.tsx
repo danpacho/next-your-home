b9695fea42b96d463d7fd8cceecbadf42ab0fa22
June@MacBook <danpa725@cau.ac.kr>
2022-04-24 19:07:11 +0900

    Add: error-404-500 pages .

---
+++ pages/404.tsx
@@ -0,0 +1,8 @@
+import { PageType } from "@/types/page/type"
+
+function ErrorPage() {
+    return <div>404</div>
+}
+
+ErrorPage.displayName = "ErrorPage" as PageType
+export default ErrorPage
---
+++ pages/500.tsx
@@ -0,0 +1,8 @@
+import { PageType } from "@/types/page/type"
+
+function ErrorPage() {
+    return <div>500</div>
+}
+
+ErrorPage.displayName = "ErrorPage" as PageType
+export default ErrorPage
