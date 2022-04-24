877527a47b991c0101a103c741f39ae1323c366f
June@MacBook <danpa725@cau.ac.kr>
2022-04-24 19:06:39 +0900

    Update: next.config.js for enable swc minify option .

--- next.config.js
+++ next.config.js
@@ -11,11 +11,15 @@ const { withPlugins } = require('next-compose-plugins')
 
 //* next default config ------------------------------
 const nextConfig = {
-  pageExtensions: ['mdx', 'tsx', 'ts'],
-  reactStrictMode: true,
-  compiler: {
-      styledComponents: true,
-  },
+    pageExtensions: ['mdx', 'tsx', 'ts'],
+    reactStrictMode: true,
+    swcMinify: true,
+    compiler: {
+        styledComponents: true,
+        // removeConsole: {
+        //     exclude: ['error'],
+        // },
+    },
 }
 
 module.exports = withPlugins(
