16f3be2a2c8aece5171ced121134e887651fcfb5
June@MacBook <danpa725@cau.ac.kr>
2022-05-01 21:40:54 +0900

    Update: MDX divider component style .

--- src/components/MDXAtoms/Divider/Divider.tsx
+++ src/components/MDXAtoms/Divider/Divider.tsx
@@ -1,9 +1,12 @@
+import { useThemeIsLight } from "@lib/atoms/theme/theme.state"
+import { IsLight } from "@typing/theme"
 import styled from "styled-components"
 
-const DividerStyled = styled.hr`
+const DividerStyled = styled.hr<IsLight>`
     width: 100%;
     height: 1.75px;
-    background-color: ${({ theme }) => theme.descriptionFontColor};
+    background-color: ${({ theme, isLight }) =>
+        isLight ? theme.gray3 : theme.gray7};
 
     border: none;
 
@@ -11,7 +14,7 @@ const DividerStyled = styled.hr`
 `
 
 function Divider() {
-    return <DividerStyled />
+    return <DividerStyled isLight={useThemeIsLight()} />
 }
 
 export default Divider
