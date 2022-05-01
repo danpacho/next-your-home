639f5d27d8a06a4e358f3051de71129e67a76723
June@MacBook <danpa725@cau.ac.kr>
2022-05-01 21:40:09 +0900

    Update: text util function .

--- src/utils/function/text.ts
+++ src/utils/function/text.ts
@@ -1,2 +1,7 @@
-export const sliceTextByMaxLength = (text: string, max: number) =>
+const sliceTextByMaxLength = (text: string, max: number) =>
     text.length <= max ? text : `${text.slice(0, max + 1)}...`
+
+const replaceUnderscoreToSpacing = (stringWithUnderscore: string) =>
+    stringWithUnderscore.replaceAll("_", " ")
+
+export { sliceTextByMaxLength, replaceUnderscoreToSpacing }
