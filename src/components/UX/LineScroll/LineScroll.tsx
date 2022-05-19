5c1681260e45a0d8b8fa40b1f3280c0d6f380b87
June@MacBook <danpa725@cau.ac.kr>
2022-05-19 13:52:46 +0900

    Update: line scroll component .

--- src/components/UX/LineScroll/LineScroll.tsx
+++ src/components/UX/LineScroll/LineScroll.tsx
@@ -1,7 +1,8 @@
-import { FontSizeType } from "@styles/utils/font"
-import media from "@styles/utils/media"
-import React, { RefObject } from "react"
 import styled from "styled-components"
+import media from "@styles/utils/media"
+import { FontSizeType } from "@styles/utils/font"
+
+import useScrollToElement from "@hooks/useScrollToElement"
 
 interface LineScrollBtnStyleProps {
     fontWeight: number
@@ -12,30 +13,34 @@ const LineScrollBtn = styled.button<LineScrollBtnStyleProps>`
     font-size: ${({ fontSize, theme }) => theme[fontSize]};
     font-weight: ${({ fontWeight }) => fontWeight};
     background-color: transparent;
+    color: ${(p) => p.theme.fontColor};
 
     ${media.widePhone} {
         display: none;
     }
 `
 interface LineScrollProps extends LineScrollBtnStyleProps {
-    scrollRef: RefObject<HTMLElement>
+    scrollRef: React.RefObject<HTMLElement>
+    children: React.ReactNode
 }
 
-function LineScroll({ fontSize, fontWeight, scrollRef }: LineScrollProps) {
-    const onClickMove = () =>
-        scrollRef.current?.scrollIntoView({
-            behavior: "smooth",
-            block: "start",
-            inline: "center",
-        })
+function LineScroll({
+    fontSize,
+    fontWeight,
+    scrollRef,
+    children: buttonText,
+}: LineScrollProps) {
+    const { scrollToElement } = useScrollToElement({
+        scrollRef,
+    })
 
     return (
         <LineScrollBtn
             fontSize={fontSize}
             fontWeight={fontWeight}
-            onClick={onClickMove}
+            onClick={scrollToElement}
         >
-            🪝
+            {buttonText}
         </LineScrollBtn>
     )
 }
