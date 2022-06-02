18124e6a4ec4322d71e596ece44d7d21c9f30cf4
June@MacBook <danpa725@cau.ac.kr>
2022-06-02 14:46:54 +0900

    Update: optimize FCP with replacing react to preact for production build .

--- next.config.js
+++ next.config.js
@@ -28,6 +28,18 @@ const nextConfig = {
         removeConsole: removeConsoleOption,
         styledComponents: true,
     },
+    webpack: (config, { dev, isServer }) => {
+        // Replace React with Preact only in client production build
+        if (!dev && !isServer) {
+            Object.assign(config.resolve.alias, {
+                react: "preact/compat",
+                "react-dom": "preact/compat",
+                "react-dom/test-utils": "preact/test-utils",
+            })
+        }
+
+        return config
+    },
 }
 
 module.exports = withPlugins([withBundleAnalyzer], nextConfig)
