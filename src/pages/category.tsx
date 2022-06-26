f3c0528a1f220f99154cc2bf4ceda98b6ff839d1
June@MacBook <danpa725@cau.ac.kr>
2022-06-26 17:04:26 +0900

    Update: grid row number style .

--- src/pages/category.tsx
+++ src/pages/category.tsx
@@ -84,8 +84,12 @@ const CategoryPageTitle = styled.div`
 `
 const CategoryContainer = styled.div`
     display: grid;
-    grid-template-columns: repeat(2, 1fr);
+    grid-template-columns: repeat(3, 1fr);
     gap: 1.5rem;
+
+    ${media.smallScreen} {
+        grid-template-columns: repeat(2, 1fr);
+    }
 
     ${media.widePhone} {
         display: flex;
