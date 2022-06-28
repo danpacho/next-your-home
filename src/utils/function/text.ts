670f39dad80df0b0422210583efa6d636d561ae4
June@MacBook <danpa725@cau.ac.kr>
2022-06-28 16:22:13 +0900

    Fix: text util length error .

--- src/utils/function/text.ts
+++ src/utils/function/text.ts
@@ -1,4 +1,4 @@
 const sliceTextByMaxLength = (text: string, max: number) =>
-    text.length <= max ? text : `${text.slice(0, max + 1)}...`
+    text.length <= max ? text : `${text.slice(0, max)}...`
 
 export { sliceTextByMaxLength }
