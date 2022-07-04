e7b6f266f8c76620d5cb9d5cc21e7e8acb960d16
June@MacBook <danpa725@cau.ac.kr>
2022-07-04 16:39:56 +0900

    Fix: typo .

--- next.config.js
+++ next.config.js
@@ -30,7 +30,7 @@ const nextConfig = {
         styledComponents: true,
     },
     webpack: (config, { dev, isServer }) => {
-        // Replace React with Preact only in client production build
+        // Replace React with Preact in production build
         if (!dev && !isServer) {
             Object.assign(config.resolve.alias, {
                 react: "preact/compat",
