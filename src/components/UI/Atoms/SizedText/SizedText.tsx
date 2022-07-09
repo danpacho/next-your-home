2142972959cd67750dfa75ff855e8f35a0b3806e
Danpa_cho <danpa725@cau.ac.kr>
2022-07-09 14:21:16 +0900

    Fix: sized text uncropping issue .

--- src/components/UI/Atoms/SizedText/SizedText.tsx
+++ src/components/UI/Atoms/SizedText/SizedText.tsx
@@ -7,6 +7,7 @@ type MediaLineOption = {
 } & {
     defaultLineNumber: number
     breakOption?: "normal" | "break-all" | "keep-all" | "break-word"
+    lineHeight: number
 }
 type SizedTextProps = MediaLineOption & {
     children: React.ReactNode
@@ -22,26 +23,58 @@ const SizedP = styled.p<MediaLineOption>`
     overflow: hidden;
 
     -webkit-line-clamp: ${(p) => p.defaultLineNumber};
+    line-height: ${(p) => p.lineHeight}rem;
+    max-height: ${({ lineHeight, defaultLineNumber }) =>
+        lineHeight * defaultLineNumber}rem;
+
     ${media.wideScreen} {
         -webkit-line-clamp: ${(p) => p.wideScreen ?? p.defaultLineNumber};
+        max-height: ${({ wideScreen, defaultLineNumber, lineHeight }) =>
+            wideScreen
+                ? lineHeight * wideScreen
+                : lineHeight * defaultLineNumber}rem;
     }
     ${media.mediumScreen} {
         -webkit-line-clamp: ${(p) => p.mediumScreen ?? p.defaultLineNumber};
+        max-height: ${({ mediumScreen, defaultLineNumber, lineHeight }) =>
+            mediumScreen
+                ? lineHeight * mediumScreen
+                : lineHeight * defaultLineNumber}rem;
     }
     ${media.smallScreen} {
         -webkit-line-clamp: ${(p) => p.smallScreen ?? p.defaultLineNumber};
+        max-height: ${({ smallScreen, defaultLineNumber, lineHeight }) =>
+            smallScreen
+                ? lineHeight * smallScreen
+                : lineHeight * defaultLineNumber}rem;
     }
     ${media.wideTablet} {
         -webkit-line-clamp: ${(p) => p.wideTablet ?? p.defaultLineNumber};
+        max-height: ${({ wideTablet, defaultLineNumber, lineHeight }) =>
+            wideTablet
+                ? lineHeight * wideTablet
+                : lineHeight * defaultLineNumber}rem;
     }
     ${media.mediumTablet} {
         -webkit-line-clamp: ${(p) => p.mediumTablet ?? p.defaultLineNumber};
+        max-height: ${({ mediumTablet, defaultLineNumber, lineHeight }) =>
+            mediumTablet
+                ? lineHeight * mediumTablet
+                : lineHeight * defaultLineNumber}rem;
     }
     ${media.widePhone} {
         -webkit-line-clamp: ${(p) => p.widePhone ?? p.defaultLineNumber};
+        max-height: ${({ widePhone, defaultLineNumber, lineHeight }) =>
+            widePhone
+                ? lineHeight * widePhone
+                : lineHeight * defaultLineNumber}rem;
     }
     ${media.mediumPhone} {
         -webkit-line-clamp: ${(p) => p.mediumPhone ?? p.defaultLineNumber};
+        max-height: ${({ mediumPhone, defaultLineNumber, lineHeight }) =>
+            mediumPhone
+                ? lineHeight * mediumPhone
+                : lineHeight * defaultLineNumber}rem;
     }
 `
 function SizedText(props: SizedTextProps) {
