3dcf5bc4d453fef91661a01207e53de395ddcd22
Danpa_cho <danpa725@cau.ac.kr>
2022-07-11 22:11:21 +0900

    Upate: at profile page, remove icon component and adapt pure img tag for total bundle-size .

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -12,10 +12,8 @@ import { getProfileSource } from "@utils/function/blog-contents-loader/profile/g
 import useSetFocusingPageColor from "@hooks/useSetFocusingPageColor"
 
 import {
-    LogoIcon,
     FacebookIcon,
     GithubIcon,
-    HeartIcon,
     InstagramIcon,
     LinkedinIcon,
     SendIcon,
@@ -37,193 +35,26 @@ export const getStaticProps: GetStaticProps<ProfileProps> = async () => {
     }
 }
 
-const ProfileContainer = styled.div`
-    display: flex;
-    flex-direction: row;
-    align-items: flex-start;
-    justify-content: space-between;
-
-    gap: 1rem;
-    width: 70%;
-    min-height: 35rem;
-
-    margin-bottom: 3rem;
-
-    ${media.mediumTablet} {
-        width: 85%;
-    }
-
-    ${media.widePhone} {
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-
-        margin-top: 2rem;
-
-        width: 100%;
-    }
-`
-
-const ProfileContentContainer = styled.div`
-    flex: 5;
-
-    ${media.widePhone} {
-        flex: unset;
-    }
-`
-
-const ProfileInfoContainer = styled.div`
-    position: sticky;
-    top: 6rem;
-
-    flex: 2;
-
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-
-    gap: 1rem;
-
-    ${media.widePhone} {
-        position: static;
-        flex: unset;
-
-        gap: 0.5rem;
-    }
-`
-
-const ProfileName = styled.p`
-    font-size: ${(p) => p.theme.xlg};
-    font-weight: 800;
-    color: ${(p) => p.theme.headerFontColor};
-    text-transform: capitalize;
-
-    margin-bottom: 0.5rem;
-
-    ${media.widePhone} {
-        margin-top: 0.5rem;
-        font-size: ${(p) => p.theme.lg};
-    }
-`
-const ProfileDivider = styled.div`
-    height: 1.25px;
-    width: 1rem;
-    background-color: ${(p) => p.theme.containerBorderColor};
-
-    margin: 0.25rem 0;
-    border-radius: ${(p) => p.theme.bxsm};
-
-    ${media.widePhone} {
-        margin: 0;
-        width: 0.75rem;
-        height: 1px;
-    }
-`
-const ProfileLogo = styled(LogoIcon)`
-    transition: all ease-out 0.1s;
-
-    filter: drop-shadow(rgba(0, 0, 0, 0.1) 0px 0px 25px -5px);
-
-    &:hover {
-        transform: scale(1.05);
-        filter: drop-shadow(rgba(0, 0, 0, 0.2) 0px 0px 5px 5px);
-    }
-
-    ${media.widePhone} {
-        width: 6.5rem;
-    }
-`
-
-const ProfileState = styled.p`
-    color: ${(p) => p.theme.descriptionFontColor};
-    font-size: ${(p) => p.theme.md};
-    font-weight: 400;
-    line-height: 1.15rem;
-    word-break: break-word;
-
-    padding: 0.35rem 0.5rem;
-
-    border-radius: ${(p) => p.theme.bmd};
-
-    border: 1px solid transparent;
-
-    &:hover {
-        border-color: ${(p) => p.theme.containerBorderColor};
-    }
-
-    ${media.widePhone} {
-    }
-`
-const ProfileTextContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    gap: 0.5rem;
-
-    ${media.widePhone} {
-        gap: 0.25rem;
-    }
-`
-
-const Copyright = styled.a`
-    all: unset;
-
-    display: flex;
-    align-items: center;
-    justify-content: flex-start;
-    gap: 0.1rem;
-
-    margin: 1rem 0;
-
-    color: ${(p) => p.theme.descriptionFontColor};
-    font-size: ${(p) => p.theme.sm};
-    font-weight: 600;
-
-    cursor: pointer;
-
-    ${(p) => iconStyle.md({ color: p.theme.red4 })};
-`
-
-interface ProfileProps {
-    profileSource: string
-}
-
-function Profile({ profileSource }: ProfileProps) {
-    useSetFocusingPageColor(config.userPallete.primary4)
-
+const ProfileLogo = ({ width, height }: { width: number; height: number }) => {
+    const [alt, fileType] = config.author.logoImageUrl.split(".")
     return (
-        <ProfileContainer>
-            <ProfileInfoContainer>
-                <ProfileLogo width={90} height={90} />
-
-                <ProfileTextContainer>
-                    <ProfileName>{config.author.name}</ProfileName>
-                    <ProfileDivider />
-                    <ProfileState>{config.author.currentState}</ProfileState>
-                    <ProfileState>{config.author.currentGoal}</ProfileState>
-                    <ProfileDivider />
-                    <ProfileContact contacts={config.author.contacts} />
-                </ProfileTextContainer>
-            </ProfileInfoContainer>
-            <ProfileContentContainer>
-                <MDXBundler mdxSource={profileSource} />
-                <Copyright href={config.author.contacts.github}>
-                    {config.copyright}
-                    <HeartIcon width="14px" height="14px" />
-                </Copyright>
-                <Copyright href="https://github.com/danpa725/next-your-home">
-                    This Blog is powered by next your home
-                    <HeartIcon width="14px" height="14px" />
-                </Copyright>
-            </ProfileContentContainer>
-        </ProfileContainer>
+        <picture>
+            <source
+                srcSet={config.author.logoImageUrl}
+                type={`image/${fileType}`}
+            />
+            {/* eslint-disable-next-line @next/next/no-img-element*/}
+            <img
+                src={config.author.logoImageUrl}
+                width={width}
+                height={height}
+                alt={alt}
+                loading="lazy"
+                decoding="async"
+            />
+        </picture>
     )
 }
-Profile.displayName = "Profile" as PageType
-
-export default Profile
 
 const ProfileButtonContainer = styled.button<IsLight>`
     transition: all cubic-bezier(0.075, 0.82, 0.165, 1) 0.5s;
@@ -327,3 +158,194 @@ const ProfileContact = ({ contacts }: Pick<AuthorInfoType, "contacts">) => {
         </ProfileContactContainer>
     )
 }
+
+const ProfileContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: flex-start;
+    justify-content: space-between;
+
+    gap: 1rem;
+    width: 70%;
+    min-height: 35rem;
+
+    margin-bottom: 3rem;
+
+    ${media.mediumTablet} {
+        width: 85%;
+    }
+
+    ${media.widePhone} {
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+
+        margin-top: 2rem;
+
+        width: 100%;
+    }
+`
+
+const ProfileContentContainer = styled.div`
+    width: 100%;
+
+    flex: 5;
+
+    ${media.widePhone} {
+        flex: unset;
+    }
+`
+
+const ProfileInfoContainer = styled.div`
+    position: sticky;
+    top: 6rem;
+
+    flex: 2;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    gap: 1rem;
+
+    ${media.widePhone} {
+        position: static;
+        flex: unset;
+
+        gap: 0.5rem;
+    }
+`
+
+const ProfileName = styled.p`
+    font-size: ${(p) => p.theme.xlg};
+    font-weight: 800;
+    color: ${(p) => p.theme.headerFontColor};
+    text-transform: capitalize;
+
+    margin-bottom: 0.5rem;
+
+    ${media.widePhone} {
+        margin-top: 0.5rem;
+        font-size: ${(p) => p.theme.lg};
+    }
+`
+const ProfileDivider = styled.div`
+    height: 1.25px;
+    width: 1rem;
+    background-color: ${(p) => p.theme.containerBorderColor};
+
+    margin: 0.25rem 0;
+    border-radius: ${(p) => p.theme.bxsm};
+
+    ${media.widePhone} {
+        margin: 0;
+        width: 0.75rem;
+        height: 1px;
+    }
+`
+const ProfileState = styled.p`
+    color: ${(p) => p.theme.descriptionFontColor};
+    font-size: ${(p) => p.theme.md};
+    font-weight: 400;
+    line-height: 1.15rem;
+    word-break: break-word;
+
+    padding: 0.35rem 0.5rem;
+
+    border-radius: ${(p) => p.theme.bmd};
+
+    border: 1px solid transparent;
+
+    &:hover {
+        border-color: ${(p) => p.theme.containerBorderColor};
+    }
+
+    ${media.widePhone} {
+        font-size: ${(p) => p.theme.sm};
+    }
+`
+const ProfileTextContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 0.5rem;
+
+    ${media.widePhone} {
+        gap: 0.25rem;
+    }
+`
+
+const CopyrightContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: center;
+    gap: 1rem;
+
+    width: 100%;
+
+    margin: 2rem 0 0.5rem 0;
+
+    color: ${(p) => p.theme.descriptionFontColor};
+    font-size: ${(p) => p.theme.sm};
+`
+
+const Copyright = styled.a`
+    all: unset;
+
+    font-weight: 700;
+    font-style: italic;
+
+    &:hover {
+        color: ${(p) => p.theme.fontColor};
+        text-decoration: underline;
+    }
+
+    cursor: pointer;
+`
+
+interface ProfileProps {
+    profileSource: string
+}
+
+function Profile({ profileSource }: ProfileProps) {
+    useSetFocusingPageColor(config.userPallete.primary4)
+
+    return (
+        <ProfileContainer>
+            <ProfileInfoContainer>
+                <ProfileTextContainer>
+                    <ProfileLogo width={110} height={110} />
+
+                    <ProfileName>{config.author.name}</ProfileName>
+
+                    <ProfileDivider />
+
+                    <ProfileState>{config.author.currentState}</ProfileState>
+                    <ProfileState>{config.author.currentGoal}</ProfileState>
+
+                    <ProfileDivider />
+
+                    <ProfileContact contacts={config.author.contacts} />
+                </ProfileTextContainer>
+            </ProfileInfoContainer>
+            <ProfileContentContainer>
+                <MDXBundler mdxSource={profileSource} />
+                <CopyrightContainer>
+                    <p>{config.copyright}</p>
+                    <p>
+                        This Blog is powered by{" "}
+                        <Copyright href="https://github.com/danpa725/next-your-home">
+                            next your home 🏠
+                        </Copyright>
+                    </p>
+                </CopyrightContainer>
+            </ProfileContentContainer>
+        </ProfileContainer>
+    )
+}
+Profile.displayName = "Profile" as PageType
+
+export default Profile
