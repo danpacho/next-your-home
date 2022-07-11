53c26da10aaee6a2d1f520e6f141980199641ae0
Danpa_cho <danpa725@cau.ac.kr>
2022-07-11 21:37:31 +0900

    Update: next.config.js .

--- next.config.js
+++ next.config.js
@@ -10,24 +10,17 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
     enabled: process.env.ANALYZE === "true",
 })
 
-//* custome plugins ---------------------------------
-
 //* next default config ------------------------------
-const removeConsoleOption =
-    process.env.NODE_ENV === "development"
-        ? false
-        : {
-              exclude: ["error"],
-          }
-
 const nextConfig = {
     pageExtensions: ["mdx", "tsx", "ts"],
     reactStrictMode: true,
     swcMinify: true,
     compiler: {
-        removeConsole: removeConsoleOption,
         reactStrictMode: true,
         styledComponents: true,
+        removeConsole: {
+            exclude: ["error"],
+        },
     },
     webpack: (config, { dev, isServer }) => {
         // Replace React with Preact in production build
