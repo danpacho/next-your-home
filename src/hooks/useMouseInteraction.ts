e2023423a6588b64abc44fa8e526e62e1ed78b5c
Danpa_cho <danpa725@cau.ac.kr>
2022-07-13 21:56:58 +0900

    Update: mouse interaction hook event to pointer for touch and mouse .

--- src/hooks/useMouseInteraction.ts
+++ src/hooks/useMouseInteraction.ts
@@ -1,15 +1,18 @@
 interface UseMouseInteractionProps {
     mouseStateSetter: (state: boolean) => void
+    neverCloseOnTouch?: boolean
 }
 
 function useMouseInteraction({
     mouseStateSetter,
+    neverCloseOnTouch,
 }: UseMouseInteractionProps): React.HTMLAttributes<HTMLElement> {
     return {
-        onMouseEnter: () => mouseStateSetter(true),
-        onMouseLeave: () => mouseStateSetter(false),
-        onTouchStart: () => mouseStateSetter(true),
-        onTouchEnd: () => mouseStateSetter(false),
+        onPointerEnter: () => mouseStateSetter(true),
+        onPointerLeave: ({ pointerType }) => {
+            if (pointerType === "touch" && neverCloseOnTouch) return
+            mouseStateSetter(false)
+        },
     }
 }
 
